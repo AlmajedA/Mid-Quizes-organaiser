6623722fe784a4990b731476d507ffa1eecefa45
shamsow <26414605+shamsow@users.noreply.github.com>
2022-11-21 22:14:44 +0900

    added blank templates

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Hey', message: 'Hello there!' })
+	res.render('home', { title: 'Home', message: 'Under Construction' })
+})
+
+app.get('/login', function (req, res) {
+	res.render('login', { title: 'Login'})
+})
+
+app.get('/listing', function (req, res) {
+	res.render('listing', { title: 'Listing'})
+})
+
+app.get('/profile', function (req, res) {
+	res.render('profile', { title: 'Profile'})
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
