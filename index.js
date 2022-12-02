41881d98f3dd298ed4d7b8b031ff67eada04b40a
Ammar <ammaralmajed7@gmail.com>
2022-12-02 17:48:55 +0300

    New features

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const Transaction = require("./models/transaction");
 
 // start db
 db.authenticate()
-  .then((result) => {
-    console.log("Connection established.");
-	
-  })
-  .catch((error) => {
-    console.log("Unable to connect to db: ", error);
-  });
+	.then((result) => {
+		console.log("Connection established.");
+
+	})
+	.catch((error) => {
+		console.log("Unable to connect to db: ", error);
+	});
 
 async function sync() {
 	await db.sync({ force: true });
@@ -29,22 +29,30 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async function (req, res) {
-	
+
 	const jane = await User.create({ username: "Jane" });
 	console.log("Jane's auto-generated ID:", jane.id);
 	res.render('home', { title: 'Home', message: jane })
 })
 
 app.get('/login', function (req, res) {
-	res.render('login', { title: 'Login'})
+	res.render('login', { title: 'Login' })
 })
 
 app.get('/listing', function (req, res) {
-	res.render('listing', { title: 'Listing'})
+	res.render('listing', { title: 'Listing' })
 })
 
 app.get('/profile', function (req, res) {
-	res.render('profile', { title: 'Profile', message: "Test"})
+	res.render('profile', { title: 'Profile', message: "Test" })
+})
+
+app.get('/pay', function (req, res) {
+	res.render('pay', { title: 'payment', message: "Test" })
+})
+
+app.get('/succeed', function (req, res) {
+	res.render('succeed', { title: 'succeed', message: "Test" })
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
